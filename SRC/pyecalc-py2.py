cdec3de3f09c831748a6ab23940e452c1ee9c774
thebakokid-does-code <80925485+thebakokid-does-code@users.noreply.github.com>
2021-06-20 15:31:38 -0500

    Added the executables and the source code.

---
+++ SRC/pyecalc-py2.py
@@ -0,0 +1,24 @@
+import time
+print("|===============================================================|")
+print("| WELCOME TO LUCAS's PYie CALCULATOR!                           |")
+print("|===============================================================|")
+print("                                                                 ")
+print("What's your name? No data will be uploaded anywhere, as this is made by a 11yr old with a 3 subscriber youtube channel.")
+name = input()
+print("Welcome to maths, " + name + "!")
+print("Addition: ")
+x = input("Type what you want to add! ")
+y = input("Another number, please? ")
+
+sum = int(x) + int(y)
+
+
+
+print("Calculating...")
+.sleep(5)
+print("This is taking aufally long..")
+sleep(10)
+print("Done!")
+sleep(1)
+print("It is ")
+print(sum)
---
+++ SRC/pyecalc-py3.py
@@ -0,0 +1,24 @@
+import time
+print("|===============================================================|")
+print("| WELCOME TO LUCAS's PYie CALCULATOR!                           |")
+print("|===============================================================|")
+print("                                                                 ")
+print("What's your name? No data will be uploaded anywhere, as this is made by a 11yr old with a 3 subscriber youtube channel.")
+name = input()
+print("Welcome to maths, " + name + "!")
+print("Addition: ")
+x = input("Type what you want to add! ")
+y = input("Another number, please? ")
+
+sum = int(x) + int(y)
+
+
+
+print("Calculating...")
+time.sleep(5)
+print("This is taking aufally long..")
+time.sleep(10)
+print("Done!")
+time.sleep(1)
+print("It is ")
+print(sum)
